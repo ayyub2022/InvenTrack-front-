4b876567cb2d342246d3074cec37482edd2bc064
partycrasher546 <muthonidennis546@gmail.com>
2024-08-07 17:30:30 +0300

    home charts

--- inventrack/src/api.jsx
+++ inventrack/src/api.jsx
@@ -5,7 +5,8 @@ const API_URL = 'http://127.0.0.1:5555';
 
 export const getTopSellingProduct = () => axios.get(`${API_URL}/top_selling_product`);
 export const getBestSellerLast7Days = () => axios.get(`${API_URL}/best_seller_last_7_days`);
-export const getTotalRevenue = () => axios.get('/total_revenue');
-export const getTotalSaleReturn = () => axios.get('/total_sale_return');
-export const getTotalPurchase = () => axios.get('/total_purchase');
-export const getTotalIncome = () => axios.get('/total_income');
+export const getTotalRevenue = () => axios.get(`${API_URL}/total_revenue`);
+export const getTotalSaleReturn = () => axios.get(`${API_URL}/total_sale_return`);
+export const getTotalPurchase = () => axios.get(`${API_URL}/total_purchase`);
+export const getTotalIncome = () => axios.get(`${API_URL}/total_income`);
+export const SalesChart = ()=> axios.get(`${API_URL}/sales_data`)
--- inventrack/src/pages/Home.jsx
+++ inventrack/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import './Home.css'; // Import the CSS file
-
+import ChartComponent from "./ChartComponent"; 
+import ChartComponentPro from "./ChartComponentPro";
 const Home = () => {
-    const [topSellingProduct, setTopSellingProduct] = useState(null);
+    
     const [bestSellerLast7Days, setBestSellerLast7Days] = useState(null);
     const [totalRevenue, setTotalRevenue] = useState(null);
     const [totalSaleReturn, setTotalSaleReturn] = useState(null);
@@ -13,17 +14,16 @@ const Home = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const topSellingProductResponse = await axios.get("http://127.0.0.1:5555/top_selling_product");
-                setTopSellingProduct(topSellingProductResponse.data);
+               
                 const bestSellerLast7DaysResponse = await axios.get("http://127.0.0.1:5555/best_seller_last_7_days");
                 setBestSellerLast7Days(bestSellerLast7DaysResponse.data);
-                const totalRevenueResponse = await axios.get("/total_revenue");
+                const totalRevenueResponse = await axios.get("http://127.0.0.1:5555/total_revenue");
                 setTotalRevenue(totalRevenueResponse.data);
                 const totalSaleReturnResponse = await axios.get("http://127.0.0.1:5555/total_sale_return");
                 setTotalSaleReturn(totalSaleReturnResponse.data);
-                const totalPurchaseResponse = await axios.get("/total_purchase");
+                const totalPurchaseResponse = await axios.get("http://127.0.0.1:5555/total_purchase");
                 setTotalPurchase(totalPurchaseResponse.data);
-                const totalIncomeResponse = await axios.get('/total_income');
+                const totalIncomeResponse = await axios.get("http://127.0.0.1:5555/total_income");
                 setTotalIncome(totalIncomeResponse.data);
             } catch (error) {
                 console.error('Error fetching data:', error);
@@ -35,10 +35,7 @@ const Home = () => {
 
     return (
         <div className="home-container">
-            <div className="card">
-                <h2>Top Selling Product</h2>
-                {topSellingProduct ? <p>{topSellingProduct.product}: {topSellingProduct.total_quantity}</p> : <p>Loading...</p>}
-            </div>
+            
             <div className="card">
                 <h2>Best Seller from Last 7 Days</h2>
                 {bestSellerLast7Days ? <p>{bestSellerLast7Days.product}: {bestSellerLast7Days.total_quantity}</p> : <p>Loading...</p>}
@@ -59,8 +56,11 @@ const Home = () => {
                 <h2>Total Income</h2>
                 {totalIncome ? <p>${totalIncome.total_income}</p> : <p>Loading...</p>}
             </div>
+            <ChartComponent/>
+            <ChartComponentPro/>
         </div>
     );
 };
 
 export default Home;
+ 
