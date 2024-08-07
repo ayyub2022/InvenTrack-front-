c467feb499a7205f6279c418b38502e1442bc3f0
NguyoVictor <victor.nguyo@student.moringaschool.com>
2024-08-07 07:52:26 +0300

    Add import statements for various components and libraries

---
+++ inventrack/src/pages/Orders/index.jsx
@@ -0,0 +1,7 @@
+import { Helmet } from "react-helmet";
+import { closeSVG } from "../../assets/images";
+import { Button, Img, Heading, Text, CheckBox, SelectBox, DatePicker, Input } from "../../components";
+import  { ReactTable } from "../../components/ReactTable";
+import Sidebar11 from "../../components/Sidebar11";
+import { createColumnHelper } from "@tanstack/react-table";
+import React from "react";
