42e96f56242f4851d227607b311a0841ad710c6e
NguyoVictor <victor.nguyo@student.moringaschool.com>
2024-08-09 08:22:05 +0300

    staged changes done

--- inventrack/src/components/Orderpage.jsx
+++ inventrack/src/components/Orderpage.jsx
@@ -155,11 +155,11 @@ export default function AddPurchaseDetails({
                             />
                           </div>
                           <div className="h-fit w-fit">
-                            {/* <Datepicker
+                            { <Datepicker
                               onChange={handleChange}
                               show={show}
                               setShow={handleClose}
-                            /> */}
+                            /> }
                             <label
                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                               htmlFor="purchaseDate"
@@ -179,13 +179,13 @@ export default function AddPurchaseDetails({
                           </div>
                         </div>
                         <div className="flex items-center space-x-4">
-                          {/* <button
+                          {<button
                             type="submit"
                             className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                           >
                             Update product
-                          </button> */}
-                          {/* <button
+                          </button> }
+                          { <button
                             type="button"
                             className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
                           >
@@ -202,7 +202,7 @@ export default function AddPurchaseDetails({
                               ></path>
                             </svg>
                             Delete
-                          </button> */}
+                          </button> }
                         </div>
                       </form>
                     </div>
