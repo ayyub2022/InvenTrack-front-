99513c688dcfdfdf0622db68769b88ed4bf18269
Abdihafith <abdyhafith@gmail.com>
2024-08-10 12:19:22 +0300

    create signup

--- inventrack/src/components/LoginpageRowOne.jsx
+++ inventrack/src/components/LoginpageRowOne.jsx
@@ -64,3 +64,58 @@ function Login() {
             }
         }
     };
+
+    // SIGNUP PAGE 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (formData.password !== formData.confirmPassword) {
+            toast.error("Passwords do not match", { position: "top-right" });
+            return;
+        }
+
+        try {
+            const response = await fetch("http://127.0.0.1:5555/adduser", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            if (response.ok) {
+                const data = await response.json();
+                toast.success("Account created successfully!", { position: "top-right", className: "toast-message" });
+                console.log("this is the data when signing up", data);
+                const { access_token } = data;
+                localStorage.setItem('access_token', access_token);
+                console.log('User registered:', data);
+
+                // Fetch the profile data to determine the role
+                const profileResponse = await profile();
+                const userRole = profileResponse.data.role;
+                console.log("this is the role", userRole);
+
+                if (userRole === 'buyer') {
+                    navigate('/my_banda');
+                } else if (userRole === 'seller') {
+                    navigate('/sellerdash');
+                } else if (userRole === 'delivery') {
+                    navigate('/driverAnalytics');
+                } else {
+                    console.log("Message after all roles");
+                    navigate('/banda_admin');
+                }
+
+                console.log('Successful');
+                navigate("/home");
+            } else {
+                toast.error("Signup failed. Please try again.", { position: "top-right", className: "toast-message" });
+                console.error('Signup failed:', response.statusText);
+            }
+        } catch (error) {
+            toast.error("Signup failed. Please try again.", { position: "top-right", className: "toast-message" });
+            console.error('Signup failed:', error);
+        }
+    };
+
+    
