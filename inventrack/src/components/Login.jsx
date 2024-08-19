42303428e909988a5da94401510d91573c888ec1
NguyoVictor <victor.nguyo@student.moringaschool.com>
2024-08-19 15:17:05 +0300

    fixed some styling in login.jsx

--- inventrack/src/components/Login.jsx
+++ inventrack/src/components/Login.jsx
@@ -1,110 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from 'react';
 import axios from 'axios';
-import './Home.css';
+import { Navigate } from 'react-router-dom';
 
-const Home = () => {
-    const formatCurrency = (value) => {
-        return new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-        }).format(value);
-    };
-    const [data, setData] = useState({
-        bestSellerLast7Days: null,
-        totalRevenue: null,
-        totalSaleReturn: null,
-        totalPurchase: null,
-        totalIncome: null
-    });
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const [bestSellerLast7DaysResponse, totalRevenueResponse, totalSaleReturnResponse, totalPurchaseResponse] = await Promise.all([
-                    axios.get("http://127.0.0.1:5555/best_seller_last_7_days"),
-                    axios.get("http://127.0.0.1:5555/total_revenue"),
-                    axios.get("http://127.0.0.1:5555/total_sale_return"),
-                    axios.get("http://127.0.0.1:5555/total_purchase"),
-
-                ]);
-
-                setData({
-                    bestSellerLast7Days: bestSellerLast7DaysResponse.data,
-                    totalRevenue: totalRevenueResponse.data,
-                    totalSaleReturn: totalSaleReturnResponse.data,
-                    totalPurchase: totalPurchaseResponse.data,
-
-                });
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    const { bestSellerLast7Days, totalRevenue, totalSaleReturn, totalPurchase,  } = data;
-
-    return (
-        <div className="home-container">
-            <header className="welcome-header">
-                <h1>Welcome to Our Dashboard</h1>
-                <p>Your one-stop solution for managing sales, revenue, and more.</p>
-                <img
-                    src="https://st.depositphotos.com/9999814/52407/i/450/depositphotos_524071248-stock-photo-smart-warehouse-management-system-with.jpg"
-                    alt="Welcome"
-                    className="welcome-image"
-                />
-            </header>
-
-            <div className="card-container">
-                <div className="card">
-                    <h2>Best Seller from Last 7 Days</h2>
-                    {bestSellerLast7Days ? (
-                        <p>{bestSellerLast7Days.product}: {bestSellerLast7Days.total_quantity}</p>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </div>
-                <div className="card">
-                    <h2>Total Revenue</h2>
-                    {totalRevenue ? (
-                        <p>{formatCurrency(totalRevenue.total_revenue)}</p>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </div>
-                <div className="card">
-                    <h2>Total Sale Return</h2>
-                    {totalSaleReturn ? (
-                        <p>{formatCurrency(totalSaleReturn.total_sale_return)}</p>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </div>
-                <div className="card">
-                    <h2>Total Purchase</h2>
-                    {totalPurchase ? (
-                        <p>{formatCurrency(totalPurchase.total_purchase)}</p>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </div>
-
-            </div>
-        </div>
-    );
-};
-
-export default Home;
-
-
- import React, { useState } from 'react';
-import axios from 'axios';
-import './Login.css';
-import { Navigate} from 'react-router-dom';
-
-const Login = ({setIsAuthenticated}) => {
+const Login = ({ setIsAuthenticated }) => {
   const [isLogin, setIsLogin] = useState(true);
   const [formData, setFormData] = useState({
     name: '',
@@ -140,8 +38,6 @@ const Login = ({setIsAuthenticated}) => {
             withCredentials: true, // Ensures cookies are sent and stored
           }
         );
-
-
       } else {
         response = await axios.post(
           'http://localhost:5555/signup',
@@ -170,72 +66,94 @@ const Login = ({setIsAuthenticated}) => {
   };
 
   return (
-    <div className="login-signup-container">
-      <div className="login-signup-form">
-        <h2>{isLogin ? 'Login' : 'Sign Up'}</h2>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+        <h2 className="text-2xl font-bold mb-6 text-center">
+          {isLogin ? 'Login' : 'Sign Up'}
+        </h2>
         <form onSubmit={handleSubmit}>
           {!isLogin && (
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
+            <div className="form-group mb-4">
+              <label htmlFor="name" className="block text-gray-700 mb-2">Name:</label>
               <input
                 type="text"
                 id="name"
                 name="name"
                 value={formData.name}
                 onChange={handleChange}
+                className="w-full p-2 border border-gray-300 rounded-lg"
                 required={!isLogin}
               />
             </div>
           )}
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
+          <div className="form-group mb-4">
+            <label htmlFor="email" className="block text-gray-700 mb-2">Email:</label>
             <input
               type="email"
               id="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-lg"
               required
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
+          <div className="form-group mb-4">
+            <label htmlFor="password" className="block text-gray-700 mb-2">Password:</label>
             <input
               type="password"
               id="password"
               name="password"
               value={formData.password}
               onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-lg"
               required
             />
           </div>
           {!isLogin && (
-            <div className="form-group">
-              <label htmlFor="role">Role:</label>
+            <div className="form-group mb-4">
+              <label htmlFor="role" className="block text-gray-700 mb-2">Role:</label>
               <select
                 id="role"
                 name="role"
                 value={formData.role}
                 onChange={handleChange}
+                className="w-full p-2 border border-gray-300 rounded-lg"
               >
                 <option value="User">User</option>
                 <option value="Admin">Admin</option>
               </select>
             </div>
           )}
-          <button type="submit" disabled={loading}>
-            {loading ? 'Processing...' : (isLogin ? 'Login' : 'Sign Up')}
-          </button>
-          {error && <div className="error-message">{error}</div>}
-          {success && <div className="success-message">{success==="Login successful"?<>
-          <Navigate to={'/'}/>
-          {setIsAuthenticated(true)}
-          </>:<></>}</div>}
+          <div className="flex justify-center mb-4">
+            <button
+              type="submit"
+              disabled={loading}
+              className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200"
+            >
+              {loading ? 'Processing...' : isLogin ? 'Login' : 'Sign Up'}
+            </button>
+          </div>
+          {error && <div className="text-red-500 text-center mb-4">{error}</div>}
+          {success && <div className="text-green-500 text-center mb-4">
+            {success === "Login successful" ? (
+              <>
+                <Navigate to="/" />
+                {setIsAuthenticated(true)}
+              </>
+            ) : (
+              success
+            )}
+          </div>}
         </form>
-        <button onClick={() => setIsLogin(!isLogin)}>
-          {isLogin ? 'Switch to Sign Up' : 'Switch to Login'}
-
-        </button>
+        <div className="flex justify-center">
+          <button
+            onClick={() => setIsLogin(!isLogin)}
+            className="text-blue-500 hover:underline"
+          >
+            {isLogin ? 'Already have an account? Sign Up' : 'Switch to Login'}
+          </button>
+        </div>
       </div>
     </div>
   );
